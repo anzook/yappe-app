8d147e1434871df4fe7a65c7d90d7813e0e09710
anz213 <56008225+anzook@users.noreply.github.com>
2020-03-15 18:13:16 -0400

    added db

---
+++ client/src/db/dogSeed.sql
@@ -0,0 +1,4 @@
+USE yappe_db;
+
+INSERT INTO PETS (name, age, sex, breed)
+VALUE ("Scruffy",  3, "male", "rotweiler"), ("Barky", 2, "female", "terrier");
---
+++ client/src/db/schema.sql
@@ -0,0 +1,6 @@
+-- Drops the application if it exists currently --
+DROP DATABASE IF EXISTS yappe_db;
+-- Creates the database for the application --
+CREATE DATABASE yappe_db;
+
+
