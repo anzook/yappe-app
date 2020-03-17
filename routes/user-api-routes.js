8565c6392f73486168ecc32165bef26936074312
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-03-17 17:34:41 -0400

    made one get route for user

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -20,20 +20,60 @@ module.exports = function (app) {
   });
 
   // route one user's info
-  app.get('/api/user/:id', ({ params }, res) => {
-    db.user.findAll({
+  app.get('/api/user/:id', async ({ params }, res) => {
+    let allInfo = []
+    const user = await db.user.findAll({
       limit: 1,
       where: {
         id: params.id
-      },
+      }
     })
-      .then(user => {
-        // send back user data in json
-        res.json(user);
-      })
       .catch(err => {
         console.log(err)
       })
+    // let userArray = {};
+    // userArray['user'] = user;
+    allInfo.push(user);
+
+    const userPets = await db.user_pet.findAll({
+      where: {
+        userId: params.id
+      }
+    })
+      .catch(err => {
+        console.log(err)
+      })
+
+
+
+    const petIDs = []
+    userPets.forEach(pet => {
+      petIDs.push(pet.petId)
+    })
+
+    const pets = await db.pet.findAll({
+      where: {
+        id: {
+          [Op.or]: petIDs
+        }
+      }
+    })
+      .catch(err => {
+        console.log(err)
+      });
+
+    pets.forEach(pet => {
+      userPets.forEach(userPet => {
+        let i = 0;
+        if (pet.id === userPet.petId) {
+          let petArray = {};
+          petArray = [pet, userPet]
+          allInfo.push(petArray);
+        }
+      })
+    })
+    console.log(allInfo[1])
+    res.json(allInfo)
   });
 
   // route to update user
@@ -54,45 +94,5 @@ module.exports = function (app) {
       .catch(err => {
         console.log(err)
       })
-  })
-
-  // route to get all pets of user
-  app.get('/api/user_pets/:id', async ({ params }, res) => {
-    const userPets = await db.user_pet.findAll({
-      where: {
-        userId: params.id
-      },
-    })
-      .catch(err => {
-        console.log(err)
-      })
-    const petIDs = []
-    userPets.forEach(pet => {
-      petIDs.push(pet.petId)
-    })
-
-    const pets = await db.pet.findAll({
-      where: {
-        id: {
-          [Op.or]: petIDs
-        }
-      }
-    })
-      .catch(err => {
-        console.log(err)
-      });
-
-    let allPets = []
-    pets.forEach(pet => {
-      userPets.forEach(userPet => {
-        if (pet.id === userPet.petId) {
-          let petArray = {};
-          petArray = [pet, userPet]
-          allPets.push(petArray);
-        }
-      })
-    })
-
-    res.json(allPets)
   })
 };
