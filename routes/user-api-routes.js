0899e80a3858526d9bf4d30d7bea3a742b2bab83
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-03-16 20:26:51 -0400

    added get route

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -1,18 +1,33 @@
 // Requiring our models and passport as we've configured it
 const db = require('../models');
 
-module.exports = function(app) {
- 
+module.exports = function (app) {
+
   app.post("/users", async (req, res) => {
     const user = await db.user.create(
       {
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
       }
-      );
-      console.log(user);
+    ).catch(err => {
+      console.log(err)
+    })
+    console.log(user);
     res.json(user);
-});
+  });
+
+  app.get('/user/:id', ({params}, res) => {
+    db.user.findAll({
+      limit: 1,
+      where: {
+        id: params.id
+      }
+    }).then(user => {
+      res.json(user);
+    }).catch(err => {
+      console.log(err)
+    })
+  })
 
 };
