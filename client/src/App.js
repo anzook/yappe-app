4452b296d90e1dbca035f5728b86d4e49e71fae4
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-03-20 16:43:33 -0400

    added API.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,29 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Dashboard from "./pages/Dashboard"
 import { BrowserRouter as Router, Route } from "react-router-dom"
+import API from './utils/API';
+
+// let array = [
+//     {
+//         "name": "Queen",
+//         "email": "Obama@email.com",
+//         "password": "qwsqwting"
+//     }
+// ]
+
+// API.createUser({
+//         "name": "Oliver",
+//         "email": "Twist@email.com",
+//         "password": "qwsqwting"
+//     })
+//     .then(res => {
+//         const userId = res.data;
+//         console.log("Userid:" + userId);
+//     })
+
+API.getPet(3)
+.then(res => {console.log(res)})
+
 
 function App() {
     return (
