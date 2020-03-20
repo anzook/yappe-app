c05767415d5a844180aa115e62ec176e791a325c
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-03-20 19:13:36 -0400

    code got deleted after pulling master; repushing

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Dashboard from "./pages/Dashboard"
 import LandingPage from "./pages/landing-page"
 import { BrowserRouter as Router, Route } from "react-router-dom"
+import API from './utils/API';
 
 function App() {
     return (
