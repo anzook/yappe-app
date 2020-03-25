6186cd5e4fa5781f2e7e9f65c7b92856d42d74fb
Hava-hizzle <mikehavrilak@gmail.com>
2020-03-24 21:17:42 -0400

    changed and unchanged markup

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -4,7 +4,6 @@ import YapNav from '../../components/navbar'
 import DogCard from '../../components/card'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
-import CareCard from '../../components/careCard'
 import YapFooter from '../../components/Footer'
 import API from '../../utils/API'
 
@@ -12,39 +11,44 @@ import API from '../../utils/API'
 
 
 class Dashboard extends Component {
-state = {
-  user: {}
-}
+  state = {
+    user: {}
+  }
   componentDidMount() {
     const userId = window.location.search.substring(1);
     API.getUser(userId)
-    .then(res => {
-      this.setState({user: res.data})
-      console.log(this.state)
-    })
+      .then(res => {
+        this.setState({ user: res.data })
+        console.log(this.state)
+      })
 
-}
+  }
   render() {
-    let cards = this.state.user[0]?.pets.map((pet)=>{
+    let cardOne = this.state.user[0]?.pets.map((pet) => {
       console.log(pet);
+
       return <div>
-          <DogCard name={pet.name} breed={pet.breed} age={pet.age} />
-            <CareCard />
-            
+        <DogCard name={pet.name} breed={pet.breed} age={pet.age} />
+      
       </div>
+
     })
+  
+                      
+ 
     return (
-      <div>
-        <YapNav />
-        <Jumbotron />
-        <Container>
-          <Row xs={2} md={4}>
-            {cards}
-          </Row>
-        </Container>
-        <YapFooter />
-      </div>
-    );
+  <div>
+    <YapNav />
+    <Jumbotron />
+    <Container>
+      <Row xs={2} md={4}>
+        {cardOne}
+        
+      </Row>
+    </Container>
+    <YapFooter />
+  </div>
+);
   }
 }
 
