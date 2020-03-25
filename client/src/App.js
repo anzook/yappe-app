486201a90da0510100e048ca0166bc2c9e1cfd2f
Hava-hizzle <mikehavrilak@gmail.com>
2020-03-25 17:46:42 -0400

    changed landing-page to Landing & updated app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Dashboard from "./pages/Dashboard"
-import LandingPage from "./pages/landing-page"
+import Landing from "./pages/Landing"
 import { BrowserRouter as Router, Route } from "react-router-dom"
-import AddDog from "./pages/add-dog";
+import AddDog from "./pages/AddDog";
 
 // let userId = 4;
 
@@ -11,7 +11,7 @@ function App() {
     return (
         <Router>
         <div>
-            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/" component={Landing} />
             <Route exact path="/add-dog" component={AddDog}/>
            <Route exact path="/dashboard" component={Dashboard} />
         </div>
