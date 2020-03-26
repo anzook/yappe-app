139822a27b17c8f0b54d590e0eca0f5502eb8453
Hava-hizzle <mikehavrilak@gmail.com>
2020-03-26 10:55:42 -0400

    added Capped folders

---
+++ client/src/components/Card/index.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+import ListGroupItem from 'react-bootstrap/ListGroup'
+import '../card/style.css';
+
+ export class NewCard extends Component {
+   render() {
+    //  const name = this.props.name
+    return (
+      <div className="card">
+      <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+  <Card.Body>
+  <ListGroup className="list-group-flush">
+    <ListGroupItem>Name: {this.props.name} </ListGroupItem>
+    <ListGroupItem>Breed: {this.props.breed} </ListGroupItem> 
+    <ListGroupItem>Age: {this.props.age} </ListGroupItem> 
+  </ListGroup> 
+  </Card.Body>
+</Card>
+</div>
+
+    )
+  }
+}
+
+export default NewCard;
+
+
+
+
+
+
+
+
+
---
+++ client/src/components/SignupForm/index.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import API from '../../utils/API';
+
+class SignupForm extends Component {
+
+    state = {
+        name: '',
+        email: '',
+        password: '',
+    }
+
+    handleInputChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value,
+            [event.target.email]: event.target.value,
+            [event.target.password]: event.target.value
+        });
+
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        console.log(this.state);
+        API.createUser({
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password
+        })
+        .then(res => {
+            console.log("This is the user id: " + res.data)
+            let userId = res.data
+            window.location.replace('/add-dog?' + userId);
+        })
+        
+    };
+
+    render() {
+        let signup = 'signup';
+        return (
+            <Form className={signup}>
+                <Form.Group>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Name"
+                        onChange={this.handleInputChange}
+                        name='name'
+                    />
+                </Form.Group>
+                <Form.Group >
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                        type="email"
+                        placeholder="Enter email"
+                        onChange={this.handleInputChange}
+                        name='email'
+                    />
+                </Form.Group>
+                <Form.Group >
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        onChange={this.handleInputChange}
+                        name='password'
+
+                    />
+                </Form.Group>
+                <Button
+                    variant="primary"
+                    type="submit"
+                    onClick={this.handleFormSubmit}
+                >
+                    Sign Up
+                </Button>
+            </Form >
+        )
+
+    }
+}
+export default SignupForm;
