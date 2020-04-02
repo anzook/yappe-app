0a3c47639f86ad798c568e8974ae9a3cad672315
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-04-02 15:05:56 -0400

    wrote function to sort and organize info so that it can be displayed in the chart

--- client/src/components/DoughnutChart/index.js
+++ client/src/components/DoughnutChart/index.js
@@ -4,26 +4,24 @@ import { Doughnut } from 'react-chartjs-2';
 import { Container, Row, Col } from 'react-bootstrap'
 import './style.css'
 
-const data = {
-	labels: [
-		'Red',
-		'Green',
-		'Yellow'
-	],
-	datasets: [{
-		data: [300, 50, 100],
-		backgroundColor: [
-			'#FF6384',
-			'#36A2EB',
-			'#FFCE56'
-		],
-		hoverBackgroundColor: [
-			'#FF6384',
-			'#36A2EB',
-			'#FFCE56'
-		]
-	}]
-};
+// const data = {
+// 	labels: [
+// 		'Bath', 'Checkup', 'Groom', 'Meds', 'Pee', 'Play', 'Poop', 'Walk',
+// 	],
+// 	datasets: [{
+// 		data: [300, 50, 100],
+// 		backgroundColor: [
+// 			'#FF6384',
+// 			'#36A2EB',
+// 			'#FFCE56'
+// 		],
+// 		hoverBackgroundColor: [
+// 			'#FF6384',
+// 			'#36A2EB',
+// 			'#FFCE56'
+// 		]
+// 	}]
+// };
 
 export default class DoughnutChart extends Component {
 
@@ -34,27 +32,89 @@ export default class DoughnutChart extends Component {
 			pet: [this.props.petInfo],
 			activities: [],
 			lastActivity: [],
-			date: null,
-			lastCaretaker: null
+			lastCaretaker: null,
+			allActivitiesTypes: [],
+			chartData: [],
 		}
 	}
 
 	componentDidMount() {
 		this.getPetActivities()
+		this.getData()
 	}
 
-	getPetActivities(){
+	getPetActivities() {
 		API.getPetActions(this.props.petId)
-		.then(res => {
-			this.setState({
-				activities: res.data,
-				lastActivity: res.data[0],
-				lastCaretaker: res.data[0].user.name
+			.then(res => {
+				let organized = res.data.map(action => {
+					return action.type
+				})
+
+				organized.sort()
+
+				this.setState({
+					activities: res.data,
+					lastActivity: res.data[0],
+					lastCaretaker: res.data[0].user.name,
+					allActivitiesTypes: organized
+
+				})
 			})
+	}
+
+	getData() {
+		let dataToChart = []
+		let current = null;
+		let cnt = 0;
+		this.state.allActivitiesTypes.forEach(type => {
+			if (type !== current) {
+				if (cnt > 0) {
+					dataToChart.push(cnt);
+					current = type;
+					cnt = 1;
+				} else {
+					current = type;
+					cnt = 1;
+				}
+			} else {
+				cnt++;
+			}
 		})
+
+		if (cnt > 0) {
+			dataToChart.push(cnt);
+		}
+
+		return dataToChart
 	}
 
 	render() {
+		let counts = this.getData()
+		const data = {
+			labels: this.state.allActivitiesTypes
+			,
+			datasets: [{
+				data: counts,
+				backgroundColor: [
+					'#1ee09d',
+					'#FF8F00',
+					'#E74C3C',
+					'#F4D03F',
+					'#3498DB',
+					'#17202A',
+					'#F4F6F7',
+				],
+				hoverBackgroundColor: [
+					'#1ee09d',
+					'#FF8F00',
+					'#E74C3C',
+					'#F4D03F',
+					'#3498DB',
+					'#17202A',
+					'#F4F6F7',
+				]
+			}]
+		}
 		return (
 			<Container className='yourDogCard'>
 				<div>
