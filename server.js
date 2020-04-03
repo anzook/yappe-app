8782b70fabbc8bd487a68a51e219a327d6f9c1a1
anz213 <56008225+anzook@users.noreply.github.com>
2020-04-03 14:46:54 -0400

    config changes take 2

--- server.js
+++ server.js
@@ -28,6 +28,9 @@ app.use(passport.session());
 // }
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 }
 else {
   app.use(express.static(path.join(__dirname, '/client/public')));
