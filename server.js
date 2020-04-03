5ea599bcdb1ae1f0bb3a11467d0f4c8f1fc5fb89
anz213 <56008225+anzook@users.noreply.github.com>
2020-04-03 15:19:02 -0400

    another version of server changes

--- server.js
+++ server.js
@@ -28,9 +28,7 @@ app.use(passport.session());
 // }
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
+ 
 }
 else {
   app.use(express.static(path.join(__dirname, '/client/public')));
