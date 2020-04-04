e829500570eb40313872322f8bc82b634283d53f
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-04-04 04:05:38 -0400

    added failsafe for when a pet has no action

--- client/src/components/DoughnutChart/index.js
+++ client/src/components/DoughnutChart/index.js
@@ -44,33 +44,7 @@ export default class DoughnutChart extends Component {
 			})
 	}
 
-	getData() {
-		let dataToChart = []
-		let current = null;
-		let cnt = 0;
-		this.state.allActivitiesTypes.forEach(type => {
-			if (type !== current) {
-				if (cnt > 0) {
-					dataToChart.push(cnt);
-					current = type;
-					cnt = 1;
-				} else {
-					current = type;
-					cnt = 1;
-				}
-			} else {
-				cnt++;
-			}
-		})
-
-		if (cnt > 0) {
-			dataToChart.push(cnt);
-		}
-
-		return dataToChart
-	}
-
-	render() {
+	checkActivities = () => {
 		let counts = this.getData();
 		let { allActivitiesTypes } = this.state;
 		let typesNoRepeats = allActivitiesTypes.filter((type, index) => allActivitiesTypes.indexOf(type) === index);
@@ -99,22 +73,61 @@ export default class DoughnutChart extends Component {
 				]
 			}]
 		}
+		if (this.state.activities.length  !== 0) {
+			return (
+				<Container className='yourDogCard'>
+
+					<div className='pet-info-div'>
+						<h4>{Functions.capitalize(this.props.petName)}</h4>
+						<ul className='removeUlStyling donut-ul'>
+							<li>Last Activity: {Functions.capitalize(this.state.lastActivity?.type)} </li>
+							<li>Logged By: {this.state.lastCaretaker}</li>
+							<li>Date: {this.state.lastActivity?.updatedAt?.slice(0, 10)}</li>
+						</ul>
+					</div>
+					<div>
+						<Doughnut data={data}
+							options={
+								{ legend: { position: "right" } }} />
+					</div>
+				</Container>
+
+			)
+		} else {
+			return (<div className='no-donut-div'>{Functions.capitalize(this.props.petName)} does not have any activities yet...</div>)
+		}
+	}
+
+	getData() {
+		let dataToChart = []
+		let current = null;
+		let cnt = 0;
+		this.state.allActivitiesTypes.forEach(type => {
+			if (type !== current) {
+				if (cnt > 0) {
+					dataToChart.push(cnt);
+					current = type;
+					cnt = 1;
+				} else {
+					current = type;
+					cnt = 1;
+				}
+			} else {
+				cnt++;
+			}
+		})
+
+		if (cnt > 0) {
+			dataToChart.push(cnt);
+		}
+
+		return dataToChart
+	}
+
+	render() {
+
 		return (
-			<Container className='yourDogCard'>
-				<div className='pet-info-div'>
-					<h4>{this.props.petName}</h4>
-					<ul className='removeUlStyling donut-ul'>
-						<li>Last Activity: {Functions.capitalize(this.state.lastActivity?.type)} </li>
-						<li>Logged By: {this.state.lastCaretaker}</li>
-						<li>Date: {this.state.lastActivity?.updatedAt?.slice(0, 10)}</li>
-					</ul>
-				</div>
-				<div>
-					<Doughnut data={data}
-						options={
-							{ legend: { position: "right" } }} />
-				</div>
-			</Container>
+			this.checkActivities()
 		)
 	};
 };
