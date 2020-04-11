4e22d6706945bc8ceb6ccc47a4ae41a19a513c75
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-04-11 15:35:31 -0400

    added bootstrap column and rows

--- client/src/components/DashboardMobile/index.js
+++ client/src/components/DashboardMobile/index.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap'
 
-export default class DashboardMobile extends Component{
+export default class DashboardMobile extends Component {
     render() {
-        return <h1>Mobile View!!!</h1>
+        return (
+            <Container fluid>
+                <Container>
+                    <Row>
+                        <Col>1 of 2</Col>
+                    </Row>
+                    <Row>
+                        <Col>1 of 3</Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Row>
+                                <Col>1 of 3</Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Container>
+            </Container>
+        )
     }
 }
