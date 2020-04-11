0d1f51ae1d408c5cd76980c0d2cd40296059a2ff
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-04-11 19:08:05 -0400

    added slected pet array to state

--- client/src/components/DashboardMobile/index.js
+++ client/src/components/DashboardMobile/index.js
@@ -20,17 +20,17 @@ export default class DashboardMobile extends Component {
         })
     }
 
-    changeDisplay = () => {
+    changeDisplay = (petInfo) => {
         let { display } = this.state;
         if (display === 'activities') {
             this.setState({
                 display: 'dog-info',
-                // petSelect: petInfo
+                petSelect: petInfo
             });
         }
         if (display === 'dog-info') {
             this.setState({
-                // petSelect: petInfo
+                petSelect: petInfo
             });
         }
     }
@@ -38,16 +38,16 @@ export default class DashboardMobile extends Component {
     renderDisplay() {
         let { display } = this.state;
         if (display === 'activities') {
-            return <h2>In First Glance</h2>
+        return <h2>In First Glance</h2>
         }
         else if (display === 'dog-info') {
-            return <h2>In Dog Profile</h2>
+            return <h2>In Dog Profile with {this.state.petSelect.name}</h2>
         }
     }
 
     render() {
         const petNames = this.state.userInfo?.pets?.map(pet => {
-            return <li key={pet.id} onClick={() => { this.changeDisplay() }}>{pet.name}</li>
+            return <li key={pet.id} onClick={() => { this.changeDisplay(pet) }}>{pet.name}</li>
         })
         return (
             <Container fluid className='mobile-view-dashboard-container'>
