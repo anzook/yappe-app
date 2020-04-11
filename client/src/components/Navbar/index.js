8a197d81291b0f7e29996d4191c2586c815131b1
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-04-11 13:12:32 -0400

    imported dog modal

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Breakpoint } from 'react-socks'
 import { Form, Navbar, Nav } from 'react-bootstrap';
+import AddDogModal from '../AddDogModal'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 import './style.css'
@@ -38,6 +39,7 @@ export class YapNav extends Component {
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto" />
+                <AddDogModal/>
                 <FontAwesomeIcon onClick={this.handleLogOff} className='icon' icon={faSignOutAlt} />
               </Navbar.Collapse>
             </Navbar>
