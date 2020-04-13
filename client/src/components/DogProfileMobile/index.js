02c5e29ce970c2de37f1a1707b97494b655e23d9
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-04-13 06:18:10 -0400

    added tabs

--- client/src/components/DogProfileMobile/index.js
+++ client/src/components/DogProfileMobile/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Container, Tab, Tabs } from 'react-bootstrap';
 import './style.css';
 
 export default class DogProfileMobile extends Component {
@@ -21,7 +22,7 @@ export default class DogProfileMobile extends Component {
         }
     }
 
-    getPet = () =>{
+    getPet = () => {
         this.setState({
             pet: this.props.pet,
             previousPetID: this.props.pet.id
@@ -30,7 +31,28 @@ export default class DogProfileMobile extends Component {
 
     render() {
         return (
-            <h2>In Dog Profile with {this.state.pet.name}</h2>
+            <Container className='dog-profile-container-mobile'>
+                <div className='dog-profile-info-mobile'>
+                    {this.state.pet.pictureLink ?
+                        <img alt='Pet photo' src={this.state.pet.pictureLink} className='dog-card-mobile-image' /> :
+                        <img alt='Pet photo' src="/images/placeholder-dog.jpg" className='dog-card-mobile-image' />}
+                    <h2>{this.state.pet.name}</h2>
+                    <div className='dog-asb-div'>
+                        <h6>Age: {this.state.pet.age}</h6>
+                        <h6>Sex: {this.state.pet.sex}</h6>
+                        <h6>Breed: {this.state.pet.breed}</h6>
+                    </div>
+                    <h5>Parent: someone</h5>
+                </div>
+                <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+                    <Tab eventKey="profile" title="Profile">
+                        <h2>profile</h2>
+                    </Tab>
+                    <Tab eventKey="post" title="Post">
+                        <h2>post</h2>
+                    </Tab>
+                </Tabs>
+            </Container>
         )
     }
 }
