ae6c4c255e26f9f4e7bdb16ccfdee5e00ce53345
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-04-13 13:59:14 -0400

    added comments

--- client/src/components/LastThreeActivitesCard/index.js
+++ client/src/components/LastThreeActivitesCard/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container } from 'react-bootstrap';
+import { Card, ListGroup } from 'react-bootstrap';
 import './style.css';
 import API from '../../utils/API';
 import Functions from '../../utils/Functions';
@@ -7,12 +7,25 @@ import Functions from '../../utils/Functions';
 export default class LastThreeActivitesCard extends Component {
     constructor(props) {
         super(props);
-        this.state = { }
+        this.state = {}
     }
 
-    render(){
-        return(
-            <h4>Last Three Activites</h4>
+    render() {
+        let activities = this.props.actions?.map(action => {
+            return <ListGroup.Item key={action.id} className='list-item-lta'>
+                <h6>Activity: {action.type}</h6>
+                <h6>Logged By: {action.user.name}</h6>
+                <h6>Date: {action.updatedAt}</h6>
+                <p>{action.detail}</p>
+            </ListGroup.Item>
+        })
+        return (
+            <Card >
+                <Card.Header>Recent Ativites</Card.Header>
+                <ListGroup variant="flush">
+                    {activities}
+                </ListGroup>
+            </Card>
         )
     }
 }
